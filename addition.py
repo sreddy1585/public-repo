e6d2bd1ba20b0a59d4cf39afd1319adcce9dbf79
S. Reddy <reddy.s@ufl.edu>
2022-10-31 23:25:11 -0400

    added subtraction

--- addition.py
+++ addition.py
@@ -3,12 +3,15 @@ def addition(a, b):
     c = a + b
     return (c)
 
+def subtraction(a, b):
+    c = a - b
+    return(c)
 
 def main():
     a = int(input("Enter a number: "))
     b = int(input("Enter another number: "))
-    print("The sum of these two numbers is ", end="")
-    print(addition(a, b))
+    print("The difference between these two numbers is ", end="")
+    print(subtraction(a, b))
 
 if __name__ == '__main__':
     main()
